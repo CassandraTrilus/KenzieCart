0f9a59eeb6b51d7f506e8624ebbad6c33302a78f
Cassandra Trilus <cassandratrilus@gmail.com>
2021-11-02 17:23:11 -0400

    added coupon code functionality

--- packages/client/src/components/CartSidebarView/index.js
+++ packages/client/src/components/CartSidebarView/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button, InputGroup, FormControl } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
 import { useUI } from 'hooks'
--- packages/client/src/components/CartSummary/index.js
+++ packages/client/src/components/CartSummary/index.js
@@ -1,7 +1,44 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { useCurrency, useProvideCart } from "hooks";
+import React from "react";
+import { useState } from "react";
+import axios from 'utils/axiosConfig.js'
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  Button,
+  FormControl,
+} from "react-bootstrap";
+import "./index";
 
 export default function CartSummary({ cartTotal }) {
+
+  const { getPrice } = useCurrency();
+  const { applyCoupon, updateCart } = useProvideCart()
+  const [promoCode, setPromoCode] = useState("")
+  const [promoMessage, setPromoMessage] = useState("")
+
+  const handleInput = (event) => {
+    setPromoCode((event.target.value).toUpperCase())
+  }
+
+  const handlePromoCode = () => {
+    axios.get(`/coupons/verify?code=${promoCode}`).then(
+      (res) => {
+        applyCoupon(res.data)
+        updateCart()
+        setPromoMessage(`Promo Code '${res.data.code}' Applied (${res.data.discount*100}% OFF)`)
+      },
+      (error) => {
+        console.log(error)
+        applyCoupon({code: "", discount: 0})
+        updateCart()
+        setPromoMessage(`Invalid Promo Code`)
+      }
+    );
+  }
+
   return (
     <div className='cart-summary'>
       <Container>
@@ -14,11 +51,25 @@ export default function CartSummary({ cartTotal }) {
           </Col>
         </Row>
         <Row className='mb-2 summary-item'>
-          <Col xs='9'>
+          <Col xs='6'>
             <p className='summary-label'>Total</p>
           </Col>
           <Col xs='3' className='text-right'>
-            <p className='summary-value'>${cartTotal}</p>
+          <p className="summary-value">{getPrice(cartTotal)} <small  className="promo-code-on font-weight-light">{promoMessage}</small></p>
+          </Col>
+          </Row>
+          <Row className="mb-2 summary-item">
+            <Col xs="9">
+              <InputGroup className="" size="sm">
+                <Button variant="outline-primary" id="button-addon1" size="sm" className="w-50 p-0 mr-1 mw-100" onClick={()=> handlePromoCode()}>
+                  Enter Promo Code
+                </Button>
+                <FormControl
+                  onChange={handleInput}
+                  aria-label="Example text with button addon"
+                  aria-describedby="basic-addon1"
+                />
+              </InputGroup>
           </Col>
         </Row>
       </Container>
